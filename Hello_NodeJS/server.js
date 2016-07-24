c841f42ee911398e7e50d7f303cc114a6c611aa7
NickPepper <nickpeppper@gmail.com>
2016-07-24 13:05:44 +0300

    indentation

--- Hello_NodeJS/server.js
+++ Hello_NodeJS/server.js
@@ -4,14 +4,14 @@ var DEBUG = require("./settings").DEBUG;
 
 function start(PORT, route, handle) {
 
-	function onRequest(request, response) {
-	  	var pathname = url.parse(request.url).pathname;
-    	DEBUG && console.log("Request for " + pathname + " received");
-    	route(handle, pathname, response);
-	}
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        DEBUG && console.log("Request for " + pathname + " received");
+        route(handle, pathname, response);
+    }
 
-	http.createServer(onRequest).listen(PORT);
-	DEBUG && console.log("Server on port " + PORT + " started OK");
+    http.createServer(onRequest).listen(PORT);
+    DEBUG && console.log("Server on port " + PORT + " started OK");
 
 }
 
