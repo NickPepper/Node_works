dc2a656d4623baf9c2f545280180b55680e4b7a5
NickPepper <nickpeppper@gmail.com>
2017-07-05 19:36:43 +0300

    hm

--- RESTful_API/todoListApi/server.js
+++ RESTful_API/todoListApi/server.js
@@ -4,7 +4,7 @@ var express     = require('express'),
     mongoose    = require('mongoose'),
     Task        = require('./api/models/todoListModel'),
     bodyParser  = require('body-parser');
-  
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb', {useMongoClient: true}); 
 
